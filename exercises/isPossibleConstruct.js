a73ad5e47688f21e98a3844892d63be7adca2ff1
nikita <nikivavlt@gmail.com>
2023-02-20 13:22:25 +0200

    Add isPossibleConstruct.js

---
+++ exercises/isPossibleConstruct.js
@@ -0,0 +1,25 @@
+/*
+Predicate function receives a set of symbols and the initial string.
+After function determine whether it is possible to construct the original string
+from a set of symbols or no.
+*/
+
+const isPossibleConstruct = (set, initialWorld) => {
+  const normalize = (world) => world.toLowerCase();
+
+  const setOfLetters = normalize(set).split('');
+
+  const test = normalize(initialWorld)
+    .split('')
+    .filter((letter) => {
+      if (setOfLetters.includes(letter)) {
+        setOfLetters.splice(setOfLetters.indexOf(letter), 1);
+        return letter;
+      }
+    })
+    .join('');
+
+  return (test === normalize(initialWorld));
+};
+
+isPossibleConstruct('scriptjava', 'javascript');
