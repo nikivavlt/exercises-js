be3796b136070dd7483d1194486eefc2e9ce3341
nikita <nikivavlt@gmail.com>
2023-02-25 09:34:38 +0200

    Update vehicleModel.js

--- oop/vehicleModel.js
+++ oop/vehicleModel.js
@@ -7,6 +7,8 @@ class Vehicle {
 
   type;
 
+  static listName = 'Vehicle list 2022'; // Static class attribute, belong to the class itself;
+
   constructor(name, color, type) {
     this.name = name;
     this.color = color;
@@ -22,9 +24,13 @@ class Vehicle {
   setFuelType(fuelType) {
     this.fuelType = fuelType;
   }
+
+  static setListName(name) { // Static method for class
+    this.listName = name;
+  }
 }
 
-Vehicle.prototype.fuelType = ''; // - Add new attributes or methods to the class through constructor prototype;
+Vehicle.prototype.fuelType = ''; // Add new attributes or methods to the class through constructor prototype;
 
 const tesla = new Vehicle('Tesla', 'Pink', 'Electric');
 const mercedes = new Vehicle('Mercedes-Benz', 'Black', 'Coupe');
@@ -32,11 +38,16 @@ const mercedes = new Vehicle('Mercedes-Benz', 'Black', 'Coupe');
 tesla.setFuelType('None');
 mercedes.setFuelType('Gasoline');
 
+Vehicle.listName = 'Vehicle list 2023'; // Change static class attribute for all Vehicle class objects;
+
+Vehicle.setListName('New vehicle list 2023');
+
 const getVehiclesList = () => {
   const newList = [...listOfVehicle].map((element) => `Name: ${element.name}
   Color: ${element.color}
   Type: ${element.type}
-  Fuel type: ${element.fuelType}`);
+  Fuel type: ${element.fuelType}
+  List name: ${Vehicle.listName}`);
 
   return newList.join('\n');
 };
