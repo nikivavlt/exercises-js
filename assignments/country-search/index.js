d45472179c82c81e775403ea2442335324f382c2
nikivavlt <nikivavlt@gmail.com>
2023-07-15 16:12:03 +0300

    Fix ESLint error in country-search

--- assignments/country-search/index.js
+++ assignments/country-search/index.js
@@ -1,29 +1,24 @@
-import countryList from './countries.js';
+import countryList from './countries';
 
 function searchCountryBy(key, keyword) {
+  let firstElement = 0;
+  let lastElement = countryList.length - 1;
 
-    let firstElement = 0;
-    let lastElement = countryList.length - 1;
+  while (firstElement <= lastElement) {
+    const middleElement = Math.floor((firstElement + lastElement) / 2);
 
-    while(firstElement <= lastElement) {
-        let middleElement = Math.floor((firstElement + lastElement ) / 2);
-        console.log(countryList[middleElement][key])
+    if (countryList[middleElement][key] === keyword) return countryList[middleElement];
 
-
-        if (countryList[middleElement][key] === keyword) return countryList[middleElement];
-
-        if (countryList[middleElement][key] > keyword) {
-            lastElement = middleElement - 1;
-        } 
-        
-        if (countryList[middleElement][key] < keyword) {
-            firstElement = middleElement + 1;
-        }
+    if (countryList[middleElement][key] > keyword) {
+      lastElement = middleElement - 1;
     }
 
-    return `${keyword} don't exists in countryList`;
+    if (countryList[middleElement][key] < keyword) {
+      firstElement = middleElement + 1;
+    }
+  }
 
+  return `${keyword} don't exists in countryList`;
 }
 
-
-console.log(searchCountryBy("id", 130));
+searchCountryBy('id', 130);
